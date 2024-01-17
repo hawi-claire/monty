1f4bc28c1591b2b269336995d678a5b479b62ad6
hawi-claire <7hawi7@gmail.com>
2024-01-17 01:06:15 -0800

    open the file

--- open_file.c
+++ open_file.c
@@ -20,3 +20,29 @@ FILE *open_my_file(const char *filename)
 		return fp;
 	}
 }
+
+/**
+ * main - prints all args it receives
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * return:always 0 (success)
+ */
+int main(int argc, char *argv[])
+
+{
+
+        int j;
+
+
+        for (j = 0; j < argc; j++)
+
+        {
+
+                printf("%s\n", argv[j]);
+
+        }
+
+        return (0);
+
+}
