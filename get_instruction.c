3dedd7515e6a7ec588ee4ad792dea9abf8a2eed3
George Ikigu <ikigu.geo@gmail.com>
2024-01-17 07:51:46 +0300

    header: add get_instruction prototype
    merge: complete rebase

--- get_instruction.c
+++ get_instruction.c
@@ -10,7 +10,6 @@
 operation_t get_instruction(char *s)
 {
 	operation_t operation;
-<<<<<<< HEAD
 	char *token, *opcode;
 	int value;
 
@@ -19,16 +18,6 @@ operation_t get_instruction(char *s)
 
 	/* todo: print to standard error. don't use printf */
 	/* todo: the string opcode should be freed by the caller*/
-=======
-	char *token, *opcode, *value_string;
-	int value;
-
-	replace_dollar_char(s);
-	opcode = malloc(sizeof(char) * 5);
-
-	/* todo: print to standard error. don't use printf */
-	/* todo: the string opcode should be freed by the caller function*/
->>>>>>> 7650eb9 (feat: add ability to tokenize the command)
 	if (!opcode)
 	{
 		printf("Error: malloc failed");
@@ -36,7 +25,7 @@ operation_t get_instruction(char *s)
 	}
 
 	token = strtok(s, " ");
-	opcode = _strcpy(opcode, token);
+	opcode = strcpy(opcode, token);
 
 	token = strtok(NULL, " ");
 
