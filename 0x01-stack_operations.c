a5b821de3ce8fc4ee1c1aff0d38db49ce6b0a3a5
George Ikigu <ikigu.geo@gmail.com>
2024-01-18 07:49:13 +0300

    feat: add ability to push to the stack

--- 0x01-stack_operations.c
+++ 0x01-stack_operations.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
   *nop- does nothing
   *@head: stack head
@@ -9,4 +10,38 @@ void nop(stack_t **head, unsigned int line_number)
 {
 	(void)head;
 	(void)line_number;
+}
+
+/**
+ * push - adds an integer to top of a stack_t stack
+ * @head: the head of a stack_t stack
+ * @line_number: the current instruction being executed
+ *
+ * Return: Nothing
+*/
+
+
+void push(stack_t **head, unsigned int line_number)
+{
+	stack_t *temp = malloc(sizeof(stack_t));
+
+	if (!temp)
+	{
+		fprint(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	temp->n = value;
+
+	if (*head == NULL)
+	{
+		*head = temp;
+		(*head)->next = NULL;
+		(*head)->prev = NULL;
+		return;
+	}
+
+	temp->next = *head;
+	temp->prev = NULL;
+	*head = temp;
 }
