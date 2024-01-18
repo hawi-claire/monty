ecc52340431a53b2102963f2b04b5cbb2e9f9b66
George Ikigu <ikigu.geo@gmail.com>
2024-01-18 06:02:54 +0300

    refactor: collapse parsing functions and main into one file

--- monty.c
+++ monty.c
@@ -110,3 +110,47 @@ void execute(operation_t operation, int line_number)
 	fprintf(stderr, "L%d: unknown instruction %s", line_number, operation.opcode);
 	exit(EXIT_FAILURE);
 }
+
+
+/**
+ * get_instruction - gets opcode and value
+ * @s: the string to extrac instruction from
+ *
+ * Return: a structure containing opcode and value
+*/
+
+operation_t get_instruction(char *s)
+{
+	operation_t operation;
+	char *token, *opcode;
+	int value;
+
+	replace_newline_char(s);
+	opcode = malloc(sizeof(char) * BUFF_SIZE);
+
+	/* todo: print to standard error. don't use printf */
+	/* todo: the string opcode should be freed by the caller*/
+	if (!opcode)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(s, " ");
+	opcode = strcpy(opcode, token);
+
+	token = strtok(NULL, " ");
+
+	if (token)
+	{
+		value = atoi(token);
+		operation.value = value;
+	}
+	else
+	{
+		operation.value = 0;
+	}
+
+	operation.opcode = opcode;
+	return (operation);
+}
