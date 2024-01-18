cd968c6449b93649c14542f8a582ef643d633612
George Ikigu <ikigu.geo@gmail.com>
2024-01-18 15:01:48 +0300

    feat: add ability to pchar

---
+++ 0x04-stack_operations.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * pchar - prints the ascii char that corresponds
+ * with value at the top of the stack
+ * @stack: the head of stack
+ * @line_number: the current line we're operating on
+ *
+ * Return - Nothing
+*/
+
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!stack)
+	{
+		fprintf("L%u: can't pchar, stack empty", line_number);
+		free_global_items();
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n > 127 || (*stack)->n < 0)
+	{
+		fprintf("L%u: can't pchar, value out of range", line_number);
+		free_global_items();
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%c\n", (*stack)->n);
+}
