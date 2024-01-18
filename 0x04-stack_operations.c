7d48281eda24a2771c31fb2eab991ec49fbbe02b
George Ikigu <ikigu.geo@gmail.com>
2024-01-18 15:30:07 +0300

    refactor: add ability for free_global_items() to also exit

--- 0x04-stack_operations.c
+++ 0x04-stack_operations.c
@@ -14,15 +14,13 @@ void pchar(stack_t **stack, unsigned int line_number)
 	if (!stack)
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
-		free_global_items();
-		exit(EXIT_FAILURE);
+		free_global_items_and_exit();
 	}
 
 	if ((*stack)->n > 127 || (*stack)->n < 0)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
-		free_global_items();
-		exit(EXIT_FAILURE);
+		free_global_items_and_exit();
 	}
 
 	printf("%c\n", (*stack)->n);
