ed330b0597794c05df7c4e6bb941675204a3aa0c
hawi-claire <7hawi7@gmail.com>
2024-01-18 12:36:14 -0800

    operations

--- 0x04-stack_operations.c
+++ 0x04-stack_operations.c
@@ -8,7 +8,6 @@
  *
  * Return - Nothing
 */
-
 void pchar(stack_t **stack, unsigned int line_number)
 {
 	if (!(*stack))
@@ -24,4 +23,91 @@ void pchar(stack_t **stack, unsigned int line_number)
 	}
 
 	printf("%c\n", (*stack)->n);
+}
+
+/**
+ * f_pstr - prints the string starting at the top of the stack,
+ * followed by a new line
+ * @stack: stack head
+ * @line_number: the current line we are operating on
+ * Return: no return
+*/
+void f_pstr(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		return;
+	}
+
+	char *str = NULL;
+
+	if (is_n_string_pointer)
+	{
+	str = (*stack)->n;
+	}
+
+	if (str != NULL)
+	{
+		while (*str != '\0')
+		{
+			putchar(*str);
+			str++;
+		}
+		putchar('\n');
+	}
+	else
+	{
+		fprintf(stderr, "L%d: invalid string type at top of stack\n", line_number);
+	}
+}
+
+/**
+  *f_rotl- rotates the stack to the top
+  *@stack: stack head
+  *@line_number: line_number
+  *Return: no return
+ */
+void f_rotl(stack_t **stack,  __attribute__((unused)) unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+
+	stack_t *temp = *stack;
+	stack_t *second = (*stack)->next;
+
+	*stack = second;
+
+	temp->next = second->next;
+	second->next = temp;
+
+	if (temp->next == NULL)
+		(*stack)->prev = temp;
+		else
+		temp->next->prev = temp;
+}
+
+/**
+  *f_rotr- rotates the stack to the bottom
+  *@stack: stack head
+  *@line_number: line_number
+  *Return: no return
+ */
+void f_rotr(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+
+	stack_t *tail = *stack;
+
+	while (tail->next != NULL)
+	tail = tail->next;
+
+	tail->prev->next = NULL;
+
+	tail->next = *stack;
+	*stack = tail;
+
+	(*stack)->prev = tail->prev;
+	tail->prev = NULL;
 }
