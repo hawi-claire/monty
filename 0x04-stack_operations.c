4c6d912f5a10c45b2e36e1b96a98bac0fc700f17
George Ikigu <ikigu.geo@gmail.com>
2024-01-18 15:03:45 +0300

    fix: pass stderr to fprint

--- 0x04-stack_operations.c
+++ 0x04-stack_operations.c
@@ -13,14 +13,14 @@ void pchar(stack_t **stack, unsigned int line_number)
 {
 	if (!stack)
 	{
-		fprintf("L%u: can't pchar, stack empty", line_number);
+		fprintf(stderr, "L%u: can't pchar, stack empty", line_number);
 		free_global_items();
 		exit(EXIT_FAILURE);
 	}
 
 	if ((*stack)->n > 127 || (*stack)->n < 0)
 	{
-		fprintf("L%u: can't pchar, value out of range", line_number);
+		fprintf(stderr, "L%u: can't pchar, value out of range", line_number);
 		free_global_items();
 		exit(EXIT_FAILURE);
 	}
